02fbae1f6cc1b0ad9d8e5142c605b19e459b8369
Nitin Meena <105815560+ANONYMOUSCHIEF@users.noreply.github.com>
2023-09-10 13:08:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (71.30%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -4,8 +4,7 @@ public:
     int countOrders(int n) {
         int mod=1e9+ 7;
         int res=1;
-        for(int i=1;i<=n;i++) res=(1LL*(res%mod)*i)%mod;
-        for(int i=1;i<2*n;i+=2) res=( 1LL*res%mod * i%mod)%mod;
+        for(int i=1;i<=n;i++) res = (1LL * res * i * (2*i-1))%mod;
         return res;
         
     }
