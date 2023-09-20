9f08a6674bab0027e317b7cb6de860d6fa34f760
Nitin Meena <105815560+ANONYMOUSCHIEF@users.noreply.github.com>
2023-09-20 11:24:08 +0530

    Time: 724 ms (5.05%), Space: 158.9 MB (16.61%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        map<int,int>m;
+        int total=accumulate(nums.begin(),nums.end(),0);
+        if(total==x) return nums.size();
+        total-=x;
+        x=total;
+        int sum=0,k=0,ans=-1;
+        m[0]=-1;
+        for(auto i:nums){
+            sum+=i;
+            if(m.find(sum-x)!=m.end()){
+                cout<<sum<<" "<<x<<" "<<k<<" "<<m[sum-x]<<"\n";
+                ans=max(ans,k-m[sum-x]);
+            }
+            if(m.find(sum)==m.end()) m[sum]=k;
+            k++;
+        }
+        if(ans==-1) return -1;
+        return nums.size()-ans;
+    }
+};
