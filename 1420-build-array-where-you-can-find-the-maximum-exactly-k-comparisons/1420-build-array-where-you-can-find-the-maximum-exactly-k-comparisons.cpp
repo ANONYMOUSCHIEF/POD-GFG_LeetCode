77c68ecdfd41eb8692839af6e722a7d6884d85de
Nitin Meena <105815560+ANONYMOUSCHIEF@users.noreply.github.com>
2023-10-07 23:53:03 +0530

    Time: 351 ms (5.53%), Space: 10.5 MB (18.43%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -7,7 +7,7 @@ public:
         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(m+1, vector<int>(k+1, -1)));
         function<int(int, int, int)> help = [&](int a, int b, int c)
         {
-            if (a == n || c<0)
+            if (a == 0 || c<0)
             {
                 if (!c)
                     return 1;
@@ -18,14 +18,14 @@ public:
             int ans = 0;
             for(int i=1;i<=b;i++)
             {
-                ans = (ans + help(a + 1, b, c)) % mod;
+                ans = (ans + help(a - 1, b, c)) % mod;
             }
              for(int i=b+1;i<=m;i++)
             {
-                ans = (ans + help(a + 1, i, c - 1)) % mod;
+                ans = (ans + help(a - 1, i, c - 1)) % mod;
             }
             return dp[a][b][c] = ans;
         };
-        return help(0,0,k);
+        return help(n,0,k);
     }
 };
