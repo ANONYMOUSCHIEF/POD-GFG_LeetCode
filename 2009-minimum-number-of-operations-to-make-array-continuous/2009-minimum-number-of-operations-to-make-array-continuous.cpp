91ee34086d37e26f69bce2c06994ea5cbb31fc89
Nitin Meena <105815560+ANONYMOUSCHIEF@users.noreply.github.com>
2023-10-11 03:25:18 +0530

    Time: 447 ms (5.76%), Space: 110 MB (5.54%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<int>& A) {
-        int N = A.size(), ans = N, j = 0;
-        sort(begin(A), end(A));
-        A.erase(unique(begin(A), end(A)), end(A));
-        int M = A.size();
-        for (int i = 0; i < M; ++i) {
-            while (j < M && A[j] < A[i] + N) ++j;
-            ans = min(ans, N - j + i);
+        set<int>s;
+        int n=A.size(),ans=n;
+        for(auto i:A){
+            s.insert(i);
+        }
+        vector<int>newV;
+        for(auto i:s){
+            newV.push_back(i);
+        }
+        for(int i=0;i<newV.size();i++){
+            int mini=newV[i];
+            int maxi=mini+n-1;
+            int j=upper_bound(newV.begin(),newV.end(),maxi)-newV.begin();
+            ans=min(ans,n-j+i);
         }
         return ans;
     }
